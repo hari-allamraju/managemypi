c5cf0aad8f9889898cafc99975a1fda557a0b6e2
Hari <anarahari@gmail.com>
2017-07-08 17:45:46 +0800

    added gallery generation code

---
+++ gallery/gallery.py
@@ -0,0 +1,45 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+ 
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+ 
+ 
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+ 
+ 
+def create_index_html():
+    hall = os.path.expanduser("~/FTP/Hall/index.html")
+    hariroom = os.path.expanduser("~/FTP/HariRoom/index.html")
+    files=[]
+    items = os.listdir(os.path.expanduser("~/FTP/Hall"))
+    for names in items:
+        if names.endswith(".jpg"):
+            files.append(names)
+    context = {
+        'files': files
+    }
+    with open(hall, 'w') as f:
+        html = render_template('index_html.j2', context)
+        f.write(html)
+    files=[]
+    items = os.listdir(os.path.expanduser("~/FTP/HariRoom"))
+    for names in items:
+        if names.endswith(".jpg"):
+            files.append(names)
+    context = {
+        'files': files
+    }
+    with open(hariroom, 'w') as f:
+        html = render_template('index_html.j2', context)
+        f.write(html)
+ 
+def main():
+    create_index_html()
+ 
+if __name__ == "__main__":
+    main()
