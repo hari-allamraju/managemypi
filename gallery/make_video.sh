10c3796fc77f638c70d1429a1ee234fda3959f4e
Hari Allamraju <anarahari@gmail.com>
2018-08-25 16:20:06 +0800

    clean up and preparing for roles

--- gallery/make_video.sh
+++
@@ -1,6 +0,0 @@
-cd ~/FTP/Hall
-rm -f *.mp4
-cat *.jpg 2>/dev/null| avconv -f image2pipe -c:v mjpeg -i - -r 25 -map 0 out.mp4
-cd ~/FTP/HariRoom
-rm -f *.mp4
-cat *.jpg 2>/dev/null| avconv -f image2pipe -c:v mjpeg -i - -r 25 -map 0 out.mp4
